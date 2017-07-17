b3f4cebab19dae5e97172d83a0cfd36bcbd9e27a
RickWeek <week.rick84@gmail.com>
2017-07-17 21:17:59 +0200

    Added some items and entities

---
+++ src/main/java/com/rickweek/items/CREEPSItemZebraHide.java
@@ -0,0 +1,12 @@
+package com.rickweek.items;
+
+import net.minecraft.item.Item;
+
+public class CREEPSItemZebraHide extends Item {
+	
+	public CREEPSItemZebraHide(String unlocalizedName, String registryName) {
+		this.setUnlocalizedName(unlocalizedName);
+		this.setRegistryName(registryName);
+		this.maxStackSize = 64;
+	}
+}
